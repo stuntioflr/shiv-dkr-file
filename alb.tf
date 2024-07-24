c4f91e2a2fe5512083ab41806205e9f30f7348c0
Fahad Khan <40563977+stuntioflr@users.noreply.github.com>
2024-07-24 23:07:27 +0530

    Update alb.tf

--- alb.tf
+++ alb.tf
@@ -1,44 +1,8 @@
-resource "aws_security_group" "outgroup" {
-  name        = "${var.environment}_${var.app_name}_to_elb"
-  description = "Allow internet traffic to hit the public network"
-  vpc_id      = var.vpc_id
+resource "aws_ecr_repository" "foo" {
+  name                 = "bar"
+  image_tag_mutability = "MUTABLE"
 
-  tags = {
-    Application = var.app_name
-    Company = "Taxfiler"
-    Domain = var.dns_name == "*" ? var.dns_zone_name : "${var.app_name}.${var.dns_zone_name}"
+  image_scanning_configuration {
+    scan_on_push = true
   }
-}
-
-resource "aws_security_group_rule" "outgroup_http_inbound" {
-  type              = "ingress"
-  from_port         = 80
-  to_port           = 80
-  protocol          = "tcp"
-  security_group_id = aws_security_group.outgroup.id
-  description       = "Allows HTTP access"
-
-  cidr_blocks = ["0.0.0.0/0"]
-}
-
-resource "aws_security_group_rule" "outgroup_https_inbound" {
-  type              = "ingress"
-  from_port         = 443
-  to_port           = 443
-  protocol          = "tcp"
-  security_group_id = aws_security_group.outgroup.id
-  description       = "Allows SSH access"
-
-  cidr_blocks = ["0.0.0.0/0"]
-}
-
-resource "aws_security_group_rule" "outgroup_egress" {
-  type              = "egress"
-  from_port         = 0
-  to_port           = 0
-  protocol          = "-1"
-  security_group_id = aws_security_group.outgroup.id
-  description       = "Allows egress access"
-
-  cidr_blocks       = ["0.0.0.0/0"]
 }
